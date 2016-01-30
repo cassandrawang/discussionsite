c9471c71c67d40e1aa62d9d69387a58588cc54d3
Cassie Wang <cassiewang@wustl.edu>
2016-01-30 20:25:28 +0000

    working basic html file and node application

---
+++ server/app.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+    path = require('path');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '../client/', 'index.html'));
+});
+
+app.listen(3000, function() {
+	console.log('listening on port 3000');
+});
