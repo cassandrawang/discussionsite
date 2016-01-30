63158032c0c14e291c22e19df487005142e509d9
Cassie Wang <cassiewang@cassies-mbp.dhcp.wustl.edu>
2016-01-30 15:06:04 -0600

    added mongoose code

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
+// dependencies
 var express = require('express'),
-    path = require('path');
+    path = require('path'),
+    mongoose = require('mongoose');
+
 var app = express();
+
+// mongoose configuration
+mongoose.connect('mongodb://localhost/test');
+
+// verify successful connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../client/', 'index.html'));
